cc4c68f11f8ec17c1fb1f5c3b31558175ee9cdf6
pollyp <pollyp@gmail.com>
2015-12-16 09:24:55 -0800

    getcurrenttab

--- src/popup.js
+++ src/popup.js
@@ -735,7 +735,8 @@ function setTabToUrl( query_url ) {
 }
 
 function xxx( query_url ) {
-  chrome.tabs.query( {url: query_url} );
+  //chrome.tabs.query( {url: query_url} );
+  chrome.tabs.getCurrent(function(Tab tab) { });
   return 7;
 }
 
