5843bc59f512dae5a8b93b1f69b0b855ea30f56e
pollyp <pollyp@gmail.com>
2015-12-16 12:24:38 -0800

    debump chromedriver

--- setup_travis.sh
+++ setup_travis.sh
@@ -17,7 +17,8 @@ else
     sudo unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
     sudo chmod a+x /usr/local/bin/chromedriver
     #google-chrome --version
-    echo "going to locate Chromium"
-    echo `sudo locate chromium-browser`
-    echo "Checking Chromium version: " + `chromium-browser --version` + "END"
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+    sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+    sudo apt-get update
+    sudo apt-get install google-chrome-stable
 fi
